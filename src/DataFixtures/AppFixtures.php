3551abe5e1a0cacc9c555172260e2f195d997a43
JyPy87 <jeremy.pigny@outlook.fr>
2021-05-11 18:33:02 +0200

    ajout Nelmio/Alice

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $loader = new NativeLoader();
+
+        //importe le fichier de fixtures et récupère les entités générés
+        $entities = $loader->loadFile(__DIR__.'/fixtures.yml')->getObjects();
+
+        //empile la liste d'objet à enregistrer en BDD
+        foreach ($entities as $entity) {
+            $manager->persist($entity);
+        };
+
+        //enregistre
+        $manager->flush();
+    }
+}
