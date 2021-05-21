0d74bd1fada353b0b6713e2036345a75f8543fcd
JyPy87 <jeremy.pigny@outlook.fr>
2021-05-21 16:00:50 +0200

    supply en cours

--- src/Controller/SupplyController.php
+++ src/Controller/SupplyController.php
@@ -11,84 +11,29 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/supply", name="supply_")
+ * @Route("/supply/", name="supply_")
  */
 class SupplyController extends AbstractController
 {
     /**
-     * @Route("/", name="index", methods={"GET"})
+     * @Route("list", name="list")
      */
-    public function index(SupplyRepository $supplyRepository): Response
+    public function list(SupplyRepository $paperRepository): Response
     {
-        return $this->render('supply/index.html.twig', [
-            'supplies' => $supplyRepository->findAll(),
+        $supplies=$paperRepository->findAll();
+        return $this->render('supply/list.html.twig', [
+            'supplies'=>$supplies,
         ]);
     }
 
     /**
-     * @Route("/new", name="new", methods={"GET","POST"})
+     * @Route("{id}", name="item", requirements={"id"="\d+"} )
      */
-    public function new(Request $request): Response
+    public function item(Supply $supply)
     {
-        $supply = new Supply();
-        $form = $this->createForm(SupplyType::class, $supply);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($supply);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('supply_index');
-        }
-
-        return $this->render('supply/new.html.twig', [
-            'supply' => $supply,
-            'form' => $form->createView(),
+        return $this->render('paper/item.html.twig',[
+            'supply'=>$supply,
         ]);
     }
-
-    /**
-     * @Route("/{id}", name="show", methods={"GET"})
-     */
-    public function show(Supply $supply): Response
-    {
-        return $this->render('supply/show.html.twig', [
-            'supply' => $supply,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Supply $supply): Response
-    {
-        $form = $this->createForm(SupplyType::class, $supply);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('supply_index');
-        }
-
-        return $this->render('supply/edit.html.twig', [
-            'supply' => $supply,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="delete", methods={"POST"})
-     */
-    public function delete(Request $request, Supply $supply): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$supply->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($supply);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('supply_index');
-    }
+    
 }
